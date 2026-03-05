fff4fd7a8d40969a6c052a10f3ff4da5bc50a59f
Nikita Shultais <nikita@shultais.ru>
2026-03-05 15:10:04 +0200

    Проверка дома на существование

--- app/api/endpoints/orders.py
+++ app/api/endpoints/orders.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi import HTTPException
 from typing import List
 
 
@@ -20,8 +21,18 @@ async def add_order(order: OrderRequest, order_service: OrderServiceDep, http_cl
 
     house_info_response = await http_client.get(f"http://127.0.0.1:8000/houses/{order.house_id}")
 
-    print(house_info_response.status_code)
-    print(house_info_response.json())
+    if house_info_response.status_code == 200:
+        house_info = house_info_response.json()
+    elif house_info_response.status_code == 404:
+        raise HTTPException(
+            status_code=422,
+            detail=[{
+                "loc": ["body", "house_id"],
+                "msg": f"Дом {order.house_id} не найден или не активен",
+                "type": "value_error.house_not_found"
+            }]
+        )
+
 
     order = order_service.build_order_from_schema(order)
     order = await order_service.add_order(order)
