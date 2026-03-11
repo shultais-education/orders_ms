65b710c9eb80c7e2509f803a724410b17c37d60d
Nikita Shultais <nikita@shultais.ru>
2026-03-11 15:12:28 +0200

    Добавил настройки

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings
-from pydantic import PostgresDsn, RedisDsn
+from pydantic import PostgresDsn
 
 
 class Settings(BaseSettings):
@@ -10,6 +10,19 @@ class Settings(BaseSettings):
     DB_PORT: int = 5432
     DB_NAME: str
     DB_ECHO: bool = False
+
+    # Настройки JWT авторизации
+    JWT_SECRET_KEY: str = ""
+    JWT_ALGORITHM: str = "HS256"
+
+    # Настройки Endpoints
+    HOUSE_INFO_ENDPOINT: str = "http://127.0.0.1:8000/api/houses"
+    MESSAGES_ENDPOINT: str = "http://127.0.0.1:8010/api/messages"
+    USER_INFO_ENDPOINT: str = "http://127.0.0.1:8030/api/users"
+
+    # Ключи доступа ко внутренним сервисам
+    SENDER_API_KEY: str = ""
+    AUTH_API_KEY: str = ""
 
     class Config:
         env_file = ".env"
